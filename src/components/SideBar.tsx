b097a0d0e58a744c331c46568754f3bf9faf6873
ThunderStormOwl <lausrock@gmail.com>
2021-04-27 16:05:35 -0300

    Sidebar component complete

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,34 @@
-export function SideBar() {
-  // Complete aqui
+import { useState } from 'react';
+import {Button} from './Button'
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps {
+  genres:GenreResponseProps[],
+  handleClick:(id:number) => void,
+  selectedGenre:Number,
+}
+
+export function SideBar(props: SideBarProps) {
+
+  return(
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+      <div className="buttons-container">
+        {props.genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => props.handleClick(genre.id)}
+            selected={props.selectedGenre === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
   devServer: {
     contentBase: path.resolve(__dirname, 'public'),
     hot: true,
+    port: 5432,
   },
   plugins: [
     isDevelopment && new ReactRefreshWebpackPlugin(),
